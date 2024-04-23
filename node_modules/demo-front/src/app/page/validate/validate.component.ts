9317dc9d28766bc7ce881bfbae5dd2a73327da16
Leonardo Miguel Aguado Diaz <leonardomiguelaguadodiaz@gmail.com>
2024-04-23 14:45:00 -0500

    Update Angular configuration and package scripts

---
+++ node_modules/demo-front/src/app/page/validate/validate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validate',
+  standalone: true,
+  imports: [],
+  templateUrl: './validate.component.html',
+  styleUrl: './validate.component.css'
+})
+export class ValidateComponent {
+
+}
