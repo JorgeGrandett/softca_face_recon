29bbfd2a8e6720e1fea099a6b8e99b8b1fb0fae2
Leonardo Miguel Aguado Diaz <leonardomiguelaguadodiaz@gmail.com>
2024-04-24 15:26:21 -0500

    Front: create component ui-button

---
+++ demo-front/src/app/ui/button/button.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'ui-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+  @Input() label: string = 'Button Tag';
+  @Input() isActive?: boolean = true;
+  @Output() onClick: EventEmitter<undefined> = new EventEmitter();
+
+  onClickHandler() {
+    this.onClick.emit();
+  }
+
+}
