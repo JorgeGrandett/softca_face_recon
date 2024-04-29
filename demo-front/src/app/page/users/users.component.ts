2c68ceb91d52676c4395dc81610ff980e489c804
Leonardo Miguel Aguado Diaz <leonardomiguelaguadodiaz@gmail.com>
2024-04-29 11:57:06 -0500

    Front: Update input actions of ui-piker and ui-input

--- demo-front/src/app/page/users/users.component.ts
+++ demo-front/src/app/page/users/users.component.ts
@@ -3,10 +3,14 @@ import { InputComponent } from '../../ui/input/input.component';
 import { ButtonComponent } from '../../ui/button/button.component';
 import { PickerComponent } from '../../ui/picker/picker.component';
 
-type userCard = {
-  title: string;
-  btnSave: string;
-  btnCancel: string;
+type UserCardData = {
+  btnSaveAllowed: boolean;
+  btnCancelAllowed: boolean;
+  user: {
+    id: string;
+    name: string;
+    photo: File | null;
+  }
 }
 
 @Component({
@@ -17,9 +21,40 @@ type userCard = {
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
-  userCardData: userCard = {
+  userCardTags = {
     title: 'User Card',
     btnSave: 'Save',
     btnCancel: 'Cancel'
+  }
+
+  userCardData: UserCardData = {
+    btnSaveAllowed: false,
+    btnCancelAllowed: false,
+    user: {
+      id: '',
+      name: '',
+      photo: null
+    }
+  }
+
+  validateUserCardData() {
+    this.userCardData.btnSaveAllowed =
+      (this.userCardData.user.id.length > 0 && this.userCardData.user.name.length > 0 && this.userCardData.user.photo !== null);
+    this.userCardData.btnCancelAllowed =
+      (this.userCardData.user.id.length > 0 || this.userCardData.user.name.length > 0 || this.userCardData.user.photo !== null);
+  }
+
+  clearUserCardData() {
+    console.log('Clearing user card data');
+    this.userCardData.user = {
+      id: '',
+      name: '',
+      photo: null
+    }
+    this.validateUserCardData();
+  }
+
+  saveUserCardData() {
+    console.log('Saving user card data:', this.userCardData.user);
   }
 }
--- demo-front/src/app/ui/picker/picker.component.ts
+++ demo-front/src/app/ui/picker/picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'ui-picker',
@@ -7,23 +7,44 @@ import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@
   templateUrl: './picker.component.html',
   styleUrl: './picker.component.css'
 })
-export class PickerComponent {
+export class PickerComponent implements OnChanges {
 
-  @ViewChild('inputId') inputId!: ElementRef<HTMLElement>;
+  @ViewChild('inputId') inputElement!: ElementRef<HTMLElement>;
   @Input() label: string = 'Image Picker';
-  @Output() fileChange: EventEmitter<File> = new EventEmitter<File>();
-  file: File | null = null;
+  @Input() file: File | null = null;
+  @Output() fileChange: EventEmitter<File | null> = new EventEmitter<File | null>();
   fileName: string = '';
 
+  ngOnChanges(changes: SimpleChanges): void {
+    //console.log('PickerComponent: onChanges');
+    const { file } = changes;
+    if (!file.currentValue && this.inputElement) {
+      this.onClearFile();
+    }
+  }
+
   onFileChange(event: any) {
+    //console.log('PickerComponent: onFileChange', event);
     this.file = event.target.files[0];
     if (this.file) {
       this.fileName = this.file.name;
-      this.inputId.nativeElement.classList.add('is-active');
+      this.inputElement.nativeElement.classList.add('is-active');
       this.fileChange.emit(this.file);
     } else {
       this.fileName = '';
-      this.inputId.nativeElement.classList.remove('is-active');
+      this.inputElement.nativeElement.classList.remove('is-active');
+    }
+  }
+
+  onClearFile() {
+    //console.log('PickerComponent: onClearFile');
+    if (!this.file) {
+      this.file = null;
+      this.fileName = '';
+      this.inputElement.nativeElement.classList.remove('is-active');
+      const inputsOnElement = this.inputElement.nativeElement.getElementsByTagName('input');
+      if (inputsOnElement.length > 0) inputsOnElement[0].value = '';
+      this.fileChange.emit(this.file);
     }
   }
 }
