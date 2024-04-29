2b39987ca9814c871b9ecff481062cfabe1209d9
Leonardo Miguel Aguado Diaz <leonardomiguelaguadodiaz@gmail.com>
2024-04-29 14:48:12 -0500

    Front: Update URL constant for user validation

--- demo-front/src/app/utils/urls.const.ts
+++ demo-front/src/app/utils/urls.const.ts
@@ -1,5 +1,5 @@
 export class Urls {
   static readonly BASE_URL = 'http://localhost:8080/api';
   static readonly USER = `${Urls.BASE_URL}/user`;
-  static readonly USERS = `${Urls.BASE_URL}/users`;
+  static readonly VALIDATE = `${Urls.BASE_URL}/validate`;
 }
