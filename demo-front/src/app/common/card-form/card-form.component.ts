0d430088c3037a0dabb1c19cac4b0954aa8a7741
Leonardo Miguel Aguado Diaz <leonardomiguelaguadodiaz@gmail.com>
2024-04-29 14:33:22 -0500

    Front: separate card component

---
+++ demo-front/src/app/common/card-form/card-form.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ButtonComponent } from '../../ui/button/button.component';
+
+@Component({
+  selector: 'app-card-form',
+  standalone: true,
+  imports: [ButtonComponent],
+  templateUrl: './card-form.component.html',
+  styleUrl: './card-form.component.css'
+})
+export class CardFormComponent {
+
+  @Input() title: string = 'Cart Title';
+  @Input() btnSave: { label: string, allowed: boolean } = {
+    label: 'Save',
+    allowed: false
+  };
+  @Input() btnCancel: { label: string, allowed: boolean } = {
+    label: 'Cancel',
+    allowed: false
+  };
+  @Output() onSave: EventEmitter<void> = new EventEmitter<void>();
+  @Output() onCancel: EventEmitter<void> = new EventEmitter<void>();
+
+  save() {
+    this.onSave.emit();
+  }
+
+  cancel() {
+    this.onCancel.emit();
+  }
+}
