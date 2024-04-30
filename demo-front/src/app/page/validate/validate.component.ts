110cadd94b3e371be2ddaa5fd005d6b98d661360
Leonardo Miguel Aguado Diaz <leonardomiguelaguadodiaz@gmail.com>
2024-04-30 09:10:23 -0500

    Front: Update alert component styles and properties, add user validation success message

--- demo-front/src/app/page/validate/validate.component.ts
+++ demo-front/src/app/page/validate/validate.component.ts
@@ -2,15 +2,23 @@ import { Component } from '@angular/core';
 import { CardFormComponent } from '../../common/card-form/card-form.component';
 import { PickerComponent } from '../../ui/picker/picker.component';
 import { ValidateService } from '../../services/validate/validate.service';
+import { AlertComponent, AlertProps } from '../../common/alert/alert.component';
+import { AlertConst } from '../../utils/alerts.const';
 
 @Component({
   selector: 'app-validate',
   standalone: true,
-  imports: [CardFormComponent, PickerComponent],
+  imports: [AlertComponent, CardFormComponent, PickerComponent],
   templateUrl: './validate.component.html',
   styleUrl: './validate.component.css'
 })
 export class ValidateComponent {
+
+  alertData: AlertProps = {
+    show: false,
+    type: 'error',
+    message: ''
+  }
 
   photo: File | null = null;
   
@@ -24,12 +32,20 @@ export class ValidateComponent {
         .subscribe({
           next: (response) => {
             console.log('Response:', response);
+            this.alertData = {
+              show: true,
+              type: 'info',
+              message: AlertConst.MSG_VALIDATE_USER
+            }
           },
           error: (error) => {
             console.error('Error:', error);
+            this.alertData = {
+              show: true,
+              type: 'error',
+              message: AlertConst.MSG_ERR_VALIDATE_USER
           }
         })
     }
   }
-
 }
--- demo-front/src/app/utils/alerts.const.ts
+++ demo-front/src/app/utils/alerts.const.ts
@@ -3,6 +3,8 @@ export class AlertConst {
   static readonly ALERT_ERR_TITLE = 'Error'
 
   static readonly MSG_CREATE_USER = 'User created successfully';
+  static readonly MSG_VALIDATE_USER = 'User updated successfully';
 
   static readonly MSG_ERR_CREATE_USER = 'Failed to create user';
+  static readonly MSG_ERR_VALIDATE_USER = 'Failed to update user';
 }
