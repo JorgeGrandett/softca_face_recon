2019470b854b20445bed2d36a927eb5ad8ffd373
Jorge Ivan Villalba Grandett <jivillalba@softcaribbean.com>
2024-05-02 09:46:29 -0500

    Se quitan imports no usados

--- demo-back/src/main/java/com/example/demoback/facerecon/controller/UserController.java
+++ demo-back/src/main/java/com/example/demoback/facerecon/controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
